f1b207b0e85491a875370d6591f47f3c0c0630ee
Andy Li <andylibc@gmail.com>
2015-09-24 21:53:37 +0000

    current_user_added

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,9 @@
 # Homepage (Root path)
+helpers do
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+end
 
 get '/' do
   erb :'index'
@@ -17,8 +22,8 @@ get '/user/results' do
   erb :'user/results'
 end
 
-post 'signup' do
-  @user = User.new(
+post '/signup' do
+  user = User.new(
     first_name: params[:first_name],
     last_name: params[:last_name],
     user_name: params[:username],
@@ -26,10 +31,16 @@ post 'signup' do
     password: params[:password],
     password_confirmation: params[:password_confirmation],
     )
-  # binding.pry
-  if @user.save
+  if user.save
+  session[:user_id] = user.id
     redirect '/'
   else
     redirect '/signup'
   end
 end
+
+get '/signin' do
+  erb :'signin'
+end
+# get "/logout"
+# end
