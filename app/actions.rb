091e9a3d91e12816de8190d669cc540fd9ab262d
warrenwegs <warrenwegner@gmail.com>
2015-09-23 21:54:27 -0700

    Update actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -4,11 +4,10 @@ get '/' do
   erb :index
 end
 
-<<<<<<< HEAD
 get "/signup" do
   erb :'users/signup'
 end
-=======
+
 get '/vote' do
   erb :vote
 end
@@ -16,4 +15,3 @@ end
 get '/user/results' do
   erb :'user/results'
 end
->>>>>>> f8f06508cc7ce3628ace21d2934957d5eda32776
