6286da08c3b1a0aa48154594ade6da05f97c854b
warrenwegs <warrenwegner@gmail.com>
2015-09-25 17:18:08 -0700

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,7 +9,6 @@ class Question < ActiveRecord::Base
   validates :category, :item1, :item2, presence: true
 
   def update_score
-    binding.pry
     total_votes = votes.count
     item1_votes = votes.where(item_id: item1.id).count
     update_attributes(item1_score: (item1_votes.to_f / total_votes.to_f).to_f) if total_votes > 0
